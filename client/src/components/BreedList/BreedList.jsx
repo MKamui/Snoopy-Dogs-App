fd634f109864a00a70dc08f7af0cabc5d506cabf
MKamui <marioelkamui@gmail.com>
2022-10-20 20:24:25 -0500

    refactor(BreedList): deploy

--- client/src/components/BreedList/BreedList.jsx
+++ client/src/components/BreedList/BreedList.jsx
@@ -11,72 +11,71 @@ const BreedList = ({ filter }) => {
 
     const { breeds } = useSelector((state) => state);
 
-    const filterFunction = () => {
-    let filtBreeds = []
-    if(breeds.length > 0) {
-        filtBreeds = breeds.filter(
-        (breed) => { return (
-        breed.name.toUpperCase().includes(filter?.name.toUpperCase()) &&
-        breed.temperament
-        ?.join(", ")
-                    .toUpperCase()
-                    .includes(filter?.temperament.toUpperCase()))
-        })}
-
-    if (filter?.sortBy === "A_FIRST") {
-        filtBreeds = filtBreeds.sort((a, b) =>
-        a.name[0].toUpperCase() > b.name[0].toUpperCase()
-            ? 1
-            : a.name[0].toUpperCase() < b.name[0].toUpperCase()
-            ? -1
-            : 0
-        );
-    }
-
-    if (filter?.sortBy === "Z_FIRST") {
-        filtBreeds = filtBreeds
-        .sort((a, b) =>
-            a.name[0].toUpperCase() > b.name[0].toUpperCase()
-            ? 1
-            : a.name[0].toUpperCase() < b.name[0].toUpperCase()
-            ? -1
-            : 0
-        )
-        .reverse();
-    }
-
-    if (filter?.sortBy === "LIGHTER") {
-        filtBreeds = filtBreeds.sort((a, b) =>
-        a.maxWeight > b.maxWeight ? 1 : a.maxWeight < b.maxWeight ? -1 : 0
-        );
-    }
-
-    if (filter?.sortBy === "HEAVIER") {
-        filtBreeds = filtBreeds
-        .sort((a, b) =>
-            a.maxWeight > b.maxWeight ? 1 : a.maxWeight < b.maxWeight ? -1 : 0
-        )
-        .reverse();
-    }
-
-    if (filter?.sortBy === "CREATED_BREED") {
-        filtBreeds = filtBreeds.filter((breed) =>
-        breed.hasOwnProperty("original")
-        );
-    }
-
-    if (filter?.sortBy === "LIGHT_25") {
-        filtBreeds = filtBreeds.filter((a) =>
-        a.maxWeight <= 25        
-        );
-    }
-
-    setLength(filtBreeds.length);
-    setFilteredBreeds(filtBreeds.slice(page * 8 - 8, page * 8));
-    };
 
     useEffect(() => {
-    if (breeds.length > 0)filterFunction()
+    if (breeds.length > 0){
+            let filtBreeds = []
+            if(breeds.length > 0) {
+                filtBreeds = breeds.filter(
+                (breed) => { return (
+                breed.name.toUpperCase().includes(filter?.name.toUpperCase()) &&
+                breed.temperament
+                ?.join(", ")
+                            .toUpperCase()
+                            .includes(filter?.temperament.toUpperCase()))
+                })}
+        
+            if (filter?.sortBy === "A_FIRST") {
+                filtBreeds = filtBreeds.sort((a, b) =>
+                a.name[0].toUpperCase() > b.name[0].toUpperCase()
+                    ? 1
+                    : a.name[0].toUpperCase() < b.name[0].toUpperCase()
+                    ? -1
+                    : 0
+                );
+            }
+        
+            if (filter?.sortBy === "Z_FIRST") {
+                filtBreeds = filtBreeds
+                .sort((a, b) =>
+                    a.name[0].toUpperCase() > b.name[0].toUpperCase()
+                    ? 1
+                    : a.name[0].toUpperCase() < b.name[0].toUpperCase()
+                    ? -1
+                    : 0
+                )
+                .reverse();
+            }
+        
+            if (filter?.sortBy === "LIGHTER") {
+                filtBreeds = filtBreeds.sort((a, b) =>
+                a.maxWeight > b.maxWeight ? 1 : a.maxWeight < b.maxWeight ? -1 : 0
+                );
+            }
+        
+            if (filter?.sortBy === "HEAVIER") {
+                filtBreeds = filtBreeds
+                .sort((a, b) =>
+                    a.maxWeight > b.maxWeight ? 1 : a.maxWeight < b.maxWeight ? -1 : 0
+                )
+                .reverse();
+            }
+        
+            if (filter?.sortBy === "CREATED_BREED") {
+                filtBreeds = filtBreeds.filter((breed) =>
+                breed.hasOwnProperty("original")
+                );
+            }
+        
+            if (filter?.sortBy === "LIGHT_25") {
+                filtBreeds = filtBreeds.filter((a) =>
+                a.maxWeight <= 25        
+                );
+            }
+        
+            setLength(filtBreeds.length);
+            setFilteredBreeds(filtBreeds.slice(page * 8 - 8, page * 8));
+        }
     }, [breeds, page, filter]);
 
     useEffect(() => {
